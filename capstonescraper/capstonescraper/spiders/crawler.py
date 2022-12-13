cfb439379b748658859af8a0b88d845dc10843e1
Ryan Vargas <ryanr99@live.com>
2022-12-13 10:19:19 -0500

    Comments add, with minor clean up

--- capstonescraper/capstonescraper/spiders/crawler.py
+++ capstonescraper/capstonescraper/spiders/crawler.py
@@ -10,31 +10,27 @@ class CrawlerSpider(scrapy.Spider):
     start_urls = ['http://www.whiskyshopusa.com/']
 
     def parse(self, response):
-        links = []
+        
+        # Display all products in li Odd class
         for products in response.css('li.Odd'):
             yield {
                 'name': products.css('a.pname::text').get(), 
                 'price': products.css('em.p-price::text').get(), 
                 'link': products.css('a').attrib['href']
             }
+        # Display all products in li Even class
         for products in response.css('li.Even'):
             yield {
                 'name': products.css('a.pname::text').get(), 
                 'price': products.css('em.p-price::text').get(), 
                 'link': products.css('a').attrib['href']
             }
-            """
-        for urls in response.css('div.SideCategoryListFlyout'):
-            for links in urls.css('a'):
-                yield { 
-                    'nextLink' : links.css('a').attrib['href']    
-                        }
-            """
         
+        # Go to next page in this div class
         for urls in response.css('div.SideCategoryListFlyout'):
             for links in urls.css('a'):
                 if 'href' in links.attrib and links.attrib['href'].startswith("http"):
                     yield response.follow(links, callback = self.parse)
         
-#products.css('a')[0].attrib['href']
+
 
