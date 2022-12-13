a919471059008e06c673a2fd245155b240750d65
Ryan Vargas <ryanr99@live.com>
2022-12-13 10:12:42 -0500

    Spider is able to go to all next page links and display information from that page and move on to the next

--- capstonescraper/capstonescraper/spiders/crawler.py
+++ capstonescraper/capstonescraper/spiders/crawler.py
@@ -1,12 +1,16 @@
+#scrapy crawl crawler -O crawlerOutput.json
+
+
 import scrapy
 import sys
 
 class CrawlerSpider(scrapy.Spider):
-    name = 'whiskyspider'
+    name = 'crawler'
     allowed_domains = ['www.whiskyshopusa.com']
     start_urls = ['http://www.whiskyshopusa.com/']
 
     def parse(self, response):
+        links = []
         for products in response.css('li.Odd'):
             yield {
                 'name': products.css('a.pname::text').get(), 
@@ -19,8 +23,18 @@ class CrawlerSpider(scrapy.Spider):
                 'price': products.css('em.p-price::text').get(), 
                 'link': products.css('a').attrib['href']
             }
+            """
         for urls in response.css('div.SideCategoryListFlyout'):
-            links = url.css('a').attrib['href']
-
+            for links in urls.css('a'):
+                yield { 
+                    'nextLink' : links.css('a').attrib['href']    
+                        }
+            """
+        
+        for urls in response.css('div.SideCategoryListFlyout'):
+            for links in urls.css('a'):
+                if 'href' in links.attrib and links.attrib['href'].startswith("http"):
+                    yield response.follow(links, callback = self.parse)
+        
 #products.css('a')[0].attrib['href']
 
