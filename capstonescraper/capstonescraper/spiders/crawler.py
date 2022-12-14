bae86fb29307604e137177d44b048197b7855866
Ryan Vargas <ryanr99@live.com>
2022-12-13 19:43:12 -0500

    MYSQL code added to crawler. Very confused not sure if Im using it correctly

--- capstonescraper/capstonescraper/spiders/crawler.py
+++ capstonescraper/capstonescraper/spiders/crawler.py
@@ -1,8 +1,8 @@
 #scrapy crawl crawler -O crawlerOutput.json
 
-
 import scrapy
 import sys
+import mysql.connector
 
 class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
@@ -10,7 +10,7 @@ class CrawlerSpider(scrapy.Spider):
     start_urls = ['http://www.whiskyshopusa.com/']
 
     def parse(self, response):
-        
+    
         # Display all products in li Odd class
         for products in response.css('li.Odd'):
             yield {
@@ -25,12 +25,42 @@ class CrawlerSpider(scrapy.Spider):
                 'price': products.css('em.p-price::text').get(), 
                 'link': products.css('a').attrib['href']
             }
+            name = products.css('a.pname::text').get(), 
+            price = products.css('em.p-price::text').get(), 
+            link = products.css('a').attrib['href']
         
         # Go to next page in this div class
-        for urls in response.css('div.SideCategoryListFlyout'):
-            for links in urls.css('a'):
+        for url in response.css('div.SideCategoryListFlyout'):
+            for links in url.css('a'):
                 if 'href' in links.attrib and links.attrib['href'].startswith("http"):
                     yield response.follow(links, callback = self.parse)
-        
 
+        # Connect to the database
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Pancho12!",
+            database="whiskyshopusa"
+        )
+
+        # Insert the extracted data into the products table
+        for products in response.css('li.Even'):
+            cursor.execute(
+                "INSERT INTO products (name, price, link) VALUES (%s, %s, %s)",
+                (products.css('a.pname::text').get()['name'], products.css('em.p-price::text').get()['price'], products.css('a').attrib['href']['link'])
+            )
+            
+        # Get the cursor
+        cursor = db.cursor()
+        
+        cursor.execute("CREATE DATABASE whiskyshopusa")
+        
+        cursor.execute("CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price VARCHAR(255), link VARCHAR(255))")
+        
+        # Commit the changes to the database
+        db.commit()
+
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
 
