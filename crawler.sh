8c1f2cc5244cf759e2955b19cc9a160233a5c318
Christopher Chambers <christopher.p.chambers@gmail.com>
2022-12-14 09:41:21 -0500

    Update crawler.sh to install mriadb-server, scrapy, and python module mysql-connector

--- crawler.sh
+++ crawler.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 /home/chamberscp/Git/crawler/crawler.py "$@"
+
+pip install scrapy
+pip install mysql-connector-python
+sudo apt install mariadb-server
+
+python3 /home/chambers/Git/crawler/capstonescraper/capstonescraper/spiders/crawler.py "$@"
