0af94efb6147b56c60cb32be7f5f87a7ec4d1d68
Ryan Vargas <ryanr99@live.com>
2022-12-14 09:57:13 -0500

    MYSQL code added again, with some additions

--- capstonescraper/capstonescraper/spiders/crawler.py
+++ capstonescraper/capstonescraper/spiders/crawler.py
@@ -1,5 +1,3 @@
-#scrapy crawl crawler -O crawlerOutput.json
-
 import scrapy
 import sys
 import mysql.connector
@@ -8,9 +6,21 @@ class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
     allowed_domains = ['www.whiskyshopusa.com']
     start_urls = ['http://www.whiskyshopusa.com/']
+
+    # Create a MySQL connection and cursor
+    db = mysql.connector.connect(
+        host="my-database.clh7uaufcnt6.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="team2project",
+        database="my-database"
+    )
+    cursor = db.cursor()
     
+    cursor.execute("CREATE DATABASE whiskyshopusa")
+    
+    cursor.execute("CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price VARCHAR(255), link VARCHAR(255))")
+
     def parse(self, response):
-    
         # Display all products in li Odd class
         for products in response.css('li.Odd'):
             yield {
@@ -18,6 +28,14 @@ class CrawlerSpider(scrapy.Spider):
                 'price': products.css('em.p-price::text').get(), 
                 'link': products.css('a').attrib['href']
             }
+            name = products.css('a.pname::text').get(), 
+            price = products.css('em.p-price::text').get(), 
+            link = products.css('a').attrib['href']
+
+            # Insert the scraped data into the "products" table in MySQL
+            self.cursor.execute("INSERT INTO products (name, price, link) VALUES (%s, %s, %s)", (name, price, link))
+            self.db.commit()
+
         # Display all products in li Even class
         for products in response.css('li.Even'):
             yield {
@@ -28,12 +46,14 @@ class CrawlerSpider(scrapy.Spider):
             name = products.css('a.pname::text').get(), 
             price = products.css('em.p-price::text').get(), 
             link = products.css('a').attrib['href']
-        
-        # Go to next page in this div class
-        for url in response.css('div.SideCategoryListFlyout'):
-            for links in url.css('a'):
-                if 'href' in links.attrib and links.attrib['href'].startswith("http"):
-                    yield response.follow(links, callback = self.parse)
-        
-
-
+            
+            # Insert the scraped data into the "products" table in MySQL
+            self.cursor.execute("INSERT INTO products (name, price, link) VALUES (%s, %s, %s)", (name, price, link))
+            self.db.commit()
+            
+            # Commit the changes to the database
+            db.commit()
+            
+            # Close the cursor and database connection
+            cursor.close()
+            db.close()
