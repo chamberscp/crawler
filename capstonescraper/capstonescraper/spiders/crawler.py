a74e3b2c39bbfee82a7dea9a55ff6560864c8e88
Ryan Vargas <ryanr99@live.com>
2022-12-20 10:05:43 -0500

    Crawler.py updated website made new changes

--- capstonescraper/capstonescraper/spiders/crawler.py
+++ capstonescraper/capstonescraper/spiders/crawler.py
@@ -1,63 +1,29 @@
 import scrapy
 import sys
 import mysql.connector
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import Rule
 
 class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
     allowed_domains = ['whiskyshopusa.com']
-    blocked_domains = open('../blocked_domains.txt').read().splitlines()
     start_urls = ['http://www.whiskyshopusa.com/']
-
-    rules = [Rule(LinkExtractor(deny_domains = (blocked_domains)), callback='parse_item', follow=False)]
     
     def parse(self, response):
-    
-        # Create a MySQL connection and cursor
-        db = mysql.connector.connect(
-            host="my-database.clh7uaufcnt6.us-east-1.rds.amazonaws.com",
-            user="admin",
-            password="team2project",
-            database="CrawlerProject"
-        )
-        cursor = db.cursor()
 
         # Display all products in li Odd class
-        for products in response.css('li.Odd'):
+        for products in response.css('div.carousel-active'):
+            for titles in products.css('h2.card-title'):
+                yield{
+                    'name': titles.css('a::text').get()
+                }   
+        for products in response.css('div.carousel-active'):
             yield {
-                'name': products.css('a.pname::text').get(), 
-                'price': products.css('em.p-price::text').get(), 
+                'price': products.css('span.price.price--withoutTax::text').get(), 
                 'link': products.css('a').attrib['href']
             }
-            names = products.css('a.pname::text').get(), 
-            prices = products.css('em.p-price::text').get(), 
-            links = products.css('a').attrib['href']
-
-            # Insert the scraped data into the "products" table in MySQL
-            query = ("INSERT INTO products(name, price, link) VALUES (%s, %s, %s)", (names, prices, links))
-            cursor.execute(query)
-            #cursor.commit()
-
-        # Display all products in li Even class
-        for products in response.css('li.Even'):
-            yield {
-                'name': products.css('a.pname::text').get(), 
-                'price': products.css('em.p-price::text').get(), 
-                'link': products.css('a').attrib['href']
-            }
-            names = products.css('a.pname::text').get(), 
-            price = products.css('em.p-price::text').get(), 
-            link = products.css('a').attrib['href']
-
+"""
         for url in response.css('div.SideCategoryListFlyout'):
             for links in url.css('a'):
                 if 'href' in links.attrib and links.attrib['href'].startswith("http"):
                     yield response.follow(links, callback = self.parse)
 
-        # Commit the changes to the database
-        db.commit()
-        
-        # Close the cursor and database connection
-        cursor.close()
-        db.close()
+"""
